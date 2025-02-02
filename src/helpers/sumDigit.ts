c39395ff050c7e36bac2f2a6ecefeb817457aa8f
Oluwademiladeogo <bickerstethdemilade@gmail.com>
2025-02-02 20:15:15 +0100

    create helper function to sum the digits in each number

---
+++ src/helpers/sumDigit.ts
@@ -0,0 +1,8 @@
+export const sumDigit = (num: number) => {
+    let sum = 0;
+    const numStr = num.toString();
+    for (let i = 0; i < numStr.length; i++) {
+        sum += parseInt(numStr[i]);
+    }
+    return sum;
+}
